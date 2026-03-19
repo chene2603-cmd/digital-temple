502a021e4e993890544c75cab9ac9c4c05dc3b39
 chene2603-cmd  <chene2603@gmail.com>
2026-03-19 12:15:52 +0800

    Update huiling.sh

--- huiling.sh
+++ huiling.sh
@@ -1,5 +1,5 @@
-git add .
-git commit -m "update"
-git push gitee main
-git push github main
-echo "✅ 发布完成"
+echo "git add ." > huiling.sh
+echo "git commit -m 'update'" >> huiling.sh
+echo "git push gitee main" >> huiling.sh
+echo "git push github main" >> huiling.sh
+echo "echo '✅ 发布完成'" >> huiling.sh
